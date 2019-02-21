ba09d93555cc98abe72e3628168533255e0b2425
Andrei_Shumski2 <Andrei_Shumski2@epam.com>
2019-02-21 09:03:58 +0300

    add styles

--- angular-news/src/app/utils/mockData.ts
+++ angular-news/src/app/utils/mockData.ts
@@ -1,6 +1,10 @@
 export const mockData = [
   {
     id: '0001',
+    source: {
+      id: 'nbc-news',
+      name: 'NBC News',
+    },
     author: 'Alan Friedman',
     title: 'Qualcomm says Apple\'s iOS update fix shouldn\'t prevent US ban on the iPhone - Phone Arena',
     description: 'Qualcomm filed a document with the U.S. ITC that says Apple\'s iOS workaround that removes any trace of unlicensed Qualcomm IP should not prevent the ITC from banning the sales and imports of certai...',
@@ -11,32 +15,30 @@ export const mockData = [
   },
   {
     id: '0002',
+    source: {
+      id: 'nbc-news',
+      name: 'NBC News',
+    },
     author: 'Alan Friedman',
     title: 'Qualcomm says Apple\'s iOS update fix shouldn\'t prevent US ban on the iPhone - Phone Arena',
     description: 'Qualcomm filed a document with the U.S. ITC that says Apple\'s iOS workaround that removes any trace of unlicensed Qualcomm IP should not prevent the ITC from banning the sales and imports of certai...',
     url: 'https://www.phonearena.com/news/ITC-to-rule-March-26th-on-possible-U.S.-iPhone-ban_id113823',
-    urlToImage: '//i-cdn.phonearena.com/images/article/113823-two/Qualcomm-says-Apples-iOS-update-fix-shouldnt-prevent-U.S.-ban-on-the-iPhone.jpg',
+    urlToImage: 'https://bloximages.newyork1.vip.townnews.com/richmond.com/content/tncms/assets/v3/editorial/0/de/0de6865d-274b-58a8-a726-863c20ab47d8/5c6c96de53ee2.image.jpg?resize=430%2C284',
     publishedAt: '2019-02-20T00:00:00Z',
     content: 'Apple has a history of disobeying court orders (eg, the UK case against Samsung). Apple probably think it owns the 9th circuit, but Trump isn\'t exactly Obama and is not going to dance to Apple\'s tune. Apple needs spanking and needs to understand that it\'s not…',
   },
   {
     id: '0003',
-    author: 'Alan Friedman',
-    title: 'Qualcomm says Apple\'s iOS update fix shouldn\'t prevent US ban on the iPhone - Phone Arena',
-    description: 'Qualcomm filed a document with the U.S. ITC that says Apple\'s iOS workaround that removes any trace of unlicensed Qualcomm IP should not prevent the ITC from banning the sales and imports of certai...',
-    url: 'https://www.phonearena.com/news/ITC-to-rule-March-26th-on-possible-U.S.-iPhone-ban_id113823',
-    urlToImage: 'https://bloximages.newyork1.vip.townnews.com/richmond.com/content/tncms/assets/v3/editorial/0/de/0de6865d-274b-58a8-a726-863c20ab47d8/5c6c96de53ee2.image.jpg?resize=430%2C284",',
-    publishedAt: '2019-02-20T00:00:00Z',
-    content: 'Apple has a history of disobeying court orders (eg, the UK case against Samsung). Apple probably think it owns the 9th circuit, but Trump isn\'t exactly Obama and is not going to dance to Apple\'s tune. Apple needs spanking and needs to understand that it\'s not…',
-  },
-  {
-    id: '0004',
-    author: 'Alan Friedman',
-    title: 'Qualcomm says Apple\'s iOS update fix shouldn\'t prevent US ban on the iPhone - Phone Arena',
-    description: 'Qualcomm filed a document with the U.S. ITC that says Apple\'s iOS workaround that removes any trace of unlicensed Qualcomm IP should not prevent the ITC from banning the sales and imports of certai...',
-    url: 'https://bloximages.newyork1.vip.townnews.com/richmond.com/content/tncms/assets/v3/editorial/0/de/0de6865d-274b-58a8-a726-863c20ab47d8/5c6c96de53ee2.image.jpg?resize=430%2C284",',
-    urlToImage: '//i-cdn.phonearena.com/images/article/113823-two/Qualcomm-says-Apples-iOS-update-fix-shouldnt-prevent-U.S.-ban-on-the-iPhone.jpg',
-    publishedAt: '2019-02-20T00:00:00Z',
-    content: 'Apple has a history of disobeying court orders (eg, the UK case against Samsung). Apple probably think it owns the 9th circuit, but Trump isn\'t exactly Obama and is not going to dance to Apple\'s tune. Apple needs spanking and needs to understand that it\'s not…',
-  },
+    source: {
+      id: null,
+      name: 'Washingtonexaminer.com'
+    },
+    author: 'https://www.washingtonexaminer.com/author/tom-joyce',
+    title: 'Manny Machado\'s contract shows the foolishness of salary caps - Washington Examiner',
+    description: 'Manny Machado made history Tuesday afternoon when news broke he had agreed to terms with the San Diego Padres on a record-breaking contract.',
+    url: 'https://www.washingtonexaminer.com/opinion/manny-machados-contract-shows-the-foolishness-of-salary-caps',
+    urlToImage: 'https://mediadc.brightspotcdn.com/dims4/default/068b5fb/2147483647/strip/true/crop/3833x2012+0+100/resize/1200x630!/quality/90/?url=https%3A%2F%2Fmediadc.brightspotcdn.com%2Fa0%2F35%2F06fae26d45b5a93512c9f7a535c7%2Fap-18311067549152.jpg',
+    publishedAt: '2019-02-19T21:51:00Z',
+    content: 'M anny Machado made history Tuesday afternoon when news broke he had agreed to terms with the San Diego Padres on a record-breaking contract. The 26-year-old third baseman and shortstop is set to sign a 10-year, $300 million deal with the club; it is the larg… [+3773 chars]'
+    }
 ];
